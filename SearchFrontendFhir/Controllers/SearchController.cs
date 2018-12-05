ec08fa61d58c159f9ef1796fe9cfcc8f47bd82e3
Tim Beaston <timbeaston@gmail.com>
2018-12-05 13:59:08 +1100

    Adding results page

--- SearchFrontendFhir/Controllers/SearchController.cs
+++ SearchFrontendFhir/Controllers/SearchController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hl7.Fhir.Model;
 using Microsoft.AspNetCore.Mvc;
+using SearchFrontendFhir.Models;
 
 namespace SearchFrontendFhir.Controllers
 {
@@ -18,10 +20,47 @@ namespace SearchFrontendFhir.Controllers
             return View();
         }
 
-        public IActionResult Results(string search)
+        public async Task<IActionResult> Results(string search)
         {
+            var FhirPath = "http://sqlonfhir-stu3.azurewebsites.net/fhir";
+            if (string.IsNullOrEmpty(search))
+            {
+                return View();
+            }
 
-            return View();
+            var fhirclient = new Hl7.Fhir.Rest.FhirClient(FhirPath);
+            //fhirclient.OnBeforeRequest += (sender, args) =>
+            //{
+            //    args.RawRequest.Headers.Add(HttpRequestHeader.Authorization,
+            //        $"Bearer {_tokenManagerService.GetAccessToken()}");
+            //};
+
+            var resultBundle = await fhirclient.SearchAsync<Practitioner>(
+                new[] { $"name={search}", "active=true" }
+            );
+            var ListofPractitioners = new List<PractitionerModel>();
+            var practitioners = resultBundle.Entry.ByResourceType<Practitioner>();
+            foreach (var prac in practitioners)
+            {
+                var ac = new PractitionerModel();
+
+                var name = prac.Name.FirstOrDefault();
+
+                if (name?.Given?.Any() ?? false)
+                {
+                    ac.Given += string.Join(" ", name.Given);
+                }
+
+                if (name?.Family?.Any() ?? false)
+                {
+                    ac.Surname += string.Join(" ", name.Family);
+                }
+
+                ListofPractitioners.Add(ac);
+            }
+
+
+            return View(ListofPractitioners);
         }
 
         
---
+++ SearchFrontendFhir/Models/PractitionerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchFrontendFhir.Models
+{
+    public class PractitionerModel
+    {
+        public string Given { get; set; }
+        public string Surname { get; set; }
+    }
+}
